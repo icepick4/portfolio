02ffb08e0007eb9eb6763db556ccfdf5a747503a
icepick4 <remi.jara@laposte.net>
2023-01-16 19:29:47 +0100

    link with projects

--- src/app/skill/skill.component.ts
+++ src/app/skill/skill.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Competence } from '../models/competence.model';
+import { Project } from '../models/project.model';
 import { CompetenceService } from '../services/competence.service';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-skill',
@@ -10,12 +12,14 @@ import { CompetenceService } from '../services/competence.service';
 })
 export class SkillComponent implements OnInit {
   skill!: Competence;
-  constructor(private route: ActivatedRoute, private competenceService: CompetenceService) {
+  projects!: Project[];
+  constructor(private route: ActivatedRoute, private competenceService: CompetenceService, private projectService: ProjectService) {
   }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.params['id'];
     this.skill = this.competenceService.getCompetenceById(id);
+    this.projects = this.skill.levels.map(level => this.projectService.getProjectById(level.projects[0]));
   }
 
 }
