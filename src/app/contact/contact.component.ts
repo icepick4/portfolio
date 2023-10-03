d0a6d54c117db66f0259be5fc726ec746217785e
Icepick <remi.jara@laposte.net>
2023-10-03 16:35:33 +0200

    adding easter-egg

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,11 +7,19 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+    count = 0;
   constructor(private title: Title) { }
 
   ngOnInit(): void {
     this.title.setTitle('Contact');
+  }
+
+  incrementCounter(){
+    this.count++;
+    if (this.count==10){
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+        this.count = 0;
+    }
   }
 
 }
